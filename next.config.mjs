662d6c68aabac9fe86cd417f1b8175e577ecbf0b
Pinka <mayhem@inbox.lv>
2025-02-22 11:53:37 +0200

    Remove Cross-Origin security headers

--- next.config.mjs
+++ next.config.mjs
@@ -62,14 +62,6 @@ const nextConfig = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "same-origin",
-          },
         ],
       },
     ];
